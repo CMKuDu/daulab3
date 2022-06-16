95eac9fbef628c0d725e42118250cee0ab46b0dd
Pineeeeeee <lethanhnhan271201@gmail.com>
2022-06-16 20:00:28 +0700

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab3_TranDau.Startup))]
+namespace Lab3_TranDau
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
